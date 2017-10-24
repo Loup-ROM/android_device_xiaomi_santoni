1dcd056950f2b699013d0c019cf2d507cafdebe7
Nikesh001 <nikkataria74@gmail.com>
2017-10-24 11:10:15 +0000

    Remove CMHW

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,12 +111,6 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
 
-# CMHW
-BOARD_USES_CYANOGEN_HARDWARE := true
-BOARD_HARDWARE_CLASS += \
-    hardware/cyanogen/cmhw \
-    device/xiaomi/santoni/cmhw
-
 # CNE / DPM
 BOARD_USES_QCNE := true
 
