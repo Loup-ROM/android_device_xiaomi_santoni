7bb0a108f6a27a0b22493320330680fdf7f5f712
Nikesh001 <nikkataria74@gmail.com>
2017-10-28 12:25:16 +0000

    initial bringup purefusion

--- fusion.mk
+++ fusion.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2015 The Android Open Source Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,12 +14,16 @@
 # limitations under the License.
 #
 
-# This file is executed by build/envsetup.sh, and can use anything
-# defined in envsetup.sh.
-#
-# In particular, you can add lunch options with the add_lunch_combo
-# function: add_lunch_combo generic-eng
+$(call inherit-product, device/xiaomi/santoni/full_santoni.mk)
 
-for var in eng user userdebug; do
-  add_lunch_combo santoni-$var
-done
+# Inherit some common PureFusionOS stuff.
+$(call inherit-product, vendor/pure/config/pure_phone.mk)
+
+PRODUCT_NAME := santoni
+BOARD_VENDOR := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="Xiaomi/santoni/santoni:7.1.2/N2G47H/7.7.6:user/release-keys" \
+    PRIVATE_BUILD_DESC="santoni-user 7.1.2 N2G47H 7.7.6 release-keys"
