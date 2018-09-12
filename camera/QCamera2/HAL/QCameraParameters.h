43b5fc8f73a93276613a0687a18974eb34b43175
bitrvmpd <e.noyer.silva@gmail.com>
2018-09-12 09:38:55 -0500

    Revert "santoni: Replace libcamera_client to android.hardware.camera.common@1.0-helper"
    This reverts commit 963bce4147dce0fbe9b048e267161dae74090703.

--- camera/QCamera2/HAL/QCameraParameters.h
+++ camera/QCamera2/HAL/QCameraParameters.h
@@ -24,7 +24,7 @@
 #include <cutils/properties.h>
 
 // System dependencies
-#include <CameraParameters.h>
+#include <camera/CameraParameters.h>
 #include <utils/Errors.h>
 
 // Camera dependencies
@@ -38,8 +38,6 @@ extern "C" {
 #include "mm_jpeg_interface.h"
 }
 
-using ::android::hardware::camera::common::V1_0::helper::CameraParameters;
-using ::android::hardware::camera::common::V1_0::helper::Size;
 using namespace android;
 
 namespace qcamera {
