b1a41435cc01fb71be29c445ccbaff3932d1fd9c
Ryan Andri <ryanandri@linuxmail.org>
2019-04-07 12:41:45 +0700

    santoni: change path of kernel tree
    * msm8937 is mean in general, so use more specific for 'santoni'

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,7 @@ BOARD_KERNEL_IMAGE_NAME 	:= Image.gz-dtb
 BOARD_KERNEL_PAGESIZE 		:=  2048
 BOARD_MKBOOTIMG_ARGS 		:= --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100
 TARGET_COMPILE_WITH_MSM_KERNEL	:= true
-TARGET_KERNEL_SOURCE 		:= kernel/xiaomi/msm8937
+TARGET_KERNEL_SOURCE 		:= kernel/xiaomi/santoni
 TARGET_EXFAT_DRIVER		:= sdfat
 TARGET_KERNEL_CONFIG		:= santoni_nontreble_defconfig
 
